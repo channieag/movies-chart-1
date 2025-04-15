2fd0c5c368a695ee5e3d136c8542dabc7a5caa03
channieag <ha.green1123@gmail.com>
2025-04-15 15:03:02 +0000

    movies

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,3 +64,20 @@ chart = (
     .properties(height=320)
 )
 st.altair_chart(chart, use_container_width=True)
+
+import matplotlib.pyplot as plt
+
+movies_data = pd.read_csv("https://raw.githubusercontent.com/nv-thang/Data-Visualization-Course/main/Dataset%20for%20Practice/movies.csv")
+movies_data.dropna()
+
+st.write("""Average Movie Budget, Grouped by Genre""")
+avg_budget = movies_data.groupby('genre')['budget'].mean().round()
+avg_budget = avg_budget.reset_index()
+genre = avg_budget['genre']
+avg_bud = avg_budget['budget']
+fig = plt.figure(figsize = (19, 10))
+plt.bar(genre, avg_bud, color = 'maroon')
+plt.xlabel('genre')
+plt.ylabel('budget')
+plt.title('Matplotlib Bar Chart Showing the Average Budget of Movies in Each Genre')
+st.pyplot(fig)
